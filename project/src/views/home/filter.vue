8bc29046306b51c9943be84f2ea9a92ce6453111
zhouli <17607619682@163.com>
2019-10-14 17:30:11 +0800

    过滤

--- project/src/views/home/filter.vue
+++ project/src/views/home/filter.vue
@@ -6,9 +6,37 @@
                 <van-dropdown-item v-model="option_value" :options="option" @change="change_lottype" />
             </van-dropdown-menu>
         </div>
-        <div>
+        <div class="fil_box">
             <span>过滤条件：</span>
+            <select class="select">
+                <option value="1">第一位落码</option>
+                <option value="2">第二位落码</option>
+            </select>
+            <img src="~@/assets/plus.png" alt="" style="width:.6rem;">
         </div>
+        <div style="color:#FF0B60;background:#F5F5F5;font-size:.32rem;padding:.27rem .56rem">
+            特殊要求加微信：xxxkd
+        </div>
+        <div style="padding:0 .56rem;">
+            <div v-for="(item,index) in list" :key="index" style="font-size:.373rem;color:#333333;padding:.3rem 0;border-bottom:2px solid #E3E3E3;line-height:.5rem">
+                <img src="~@/assets/min.png" alt="" style="width:.48rem;">
+                 第1位落码
+            </div>
+        </div>
+        <div class="text_center" style="padding:.35rem 0;">
+            <van-button style="width:4.03rem;height:1.21rem;background:#87AC55;border-radius:.2rem;color:#fff;font-size:0.53rem;line-height:0.8rem;">过滤</van-button>
+        </div>
+        <div style="background:#F5F5F5;height:0.27rem;"></div>
+        <div style="padding:.4rem .56rem;">
+            <div style="padding-bottom:.27rem;">
+                <span style="font-size:0.37rem;color:#333333">过滤结果</span> 
+                <span style="float:right;font-size:0.37rem;color:#FF0B60">复制</span>
+            </div>
+            <div style="font-size:0.29rem;color:#333333;line-height:0.6rem;text-align:justify">
+                1123   2313   3131  4567   334   8272   2292   4242   4232   1123   2313   3131  4567   334   8272   2292   4242   4232   4334   2313   3131  4567   334   8272   2292   4242   4232   1123   2313   3131  4567   334   8272   2292   4242   4232   4334   2313   3131  4567   334   8272   2292   4242   4232   1123   2313   3131  4567   334   8272   2292   4242   4232   4334   2313   3131  4567   334   8272   2292   4242   4232   1123   2313   3131  4567   334   8272   2292   4242   4232   4334   2313   3131  4567   334   8272   2292   4242   4232   1123   2313   3131  4567   334   8272   2292   4242   4232   4334
+            </div>
+        </div>
+        
        
         
     </div>
@@ -24,6 +52,10 @@ export default {
                 { text: '七星彩1', value: 'b' },
                 { text: '七星彩2', value: 'c' },
             ],
+
+            list:[
+                {},{},{},{}
+            ]
         }
     },
     methods:{
@@ -40,6 +72,17 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
+.fil_box
+    padding .12rem .56rem
+.select
+    background: #fff;
+    border: 2px solid #E3E3E3;
+    border-radius: .12rem;
+    outline: none;
+    width: 4.67rem;
+    height: .92rem;
+    text-align: center;
+    padding: 0 1rem;
 // 右上角下拉菜单样式 start
 /deep/ .van-dropdown-item.van-dropdown-item--down
     top 50px !important
